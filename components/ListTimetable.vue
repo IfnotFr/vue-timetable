d6b7ee8925c82f89e14909ce94ce372439d1a13d
Favre Anael <anael.favre@gmail.com>
2017-08-10 13:33:16 +0200

    Big Bang

---
+++ components/ListTimetable.vue
@@ -0,0 +1,79 @@
+<template>
+  <div class="vue-timetable vue-timetable-list-mode">
+    <div v-for="day in date.list" v-if="day.events.length > 0" class="vue-timetable-day-container">
+      <slot name="header" :day="day">
+        <header>
+          {{ day.date | moment("dddd DD") }}
+          {{ day.date | moment("MMMM") }}
+          <span>{{ day.date | moment("DD/MM/YYYY") }}</span>
+        </header>
+      </slot>
+      <ul>
+        <li v-for="event in day.events">
+          <span class="vue-timetable-event-color" :style="{ backgroundColor: event.color }"></span>
+          <a class="vue-timetable-event-link" href="#">
+            <strong class="vue-timetable-event-time">{{ event.date.from | moment('HH:mm') }}</strong>
+            <span class="vue-timetable-event-name">
+              <slot :event="event">
+                {{ event.text }}
+              </slot>
+            </span>
+          </a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script>
+  import moment from 'moment'
+
+  export default {
+    name: 'list-timetable',
+    props: ['from', 'to', 'events', 'reverse'],
+    data () {
+      return {
+        date: {
+          from: moment(this.from),
+          to: typeof this.to === 'undefined' ? moment().add(30, 'days') : moment(this.to),
+          list: []
+        }
+      }
+    },
+    watch: {
+      events () {
+        this.fillDaysList()
+      }
+    },
+    methods: {
+      fillDaysList () {
+        this.date.list = []
+
+        let now = this.date.from.clone()
+        while (now.isBefore(this.date.to) || now.isSame(this.date.to)) {
+          this.date.list.push({
+            date: now.format(),
+            events: this.getDayEvents(now)
+          })
+          now.add(1, 'days')
+        }
+
+        if (this.reverse) {
+          this.date.list.reverse()
+        }
+      },
+      getDayEvents (day) {
+        if (typeof this.events === 'undefined') {
+          return []
+        }
+
+        return this.events.filter((event) => {
+          return day.isSame(event.date.from, 'day')
+        })
+      }
+    },
+    created () {
+      this.fillDaysList()
+    }
+  }
+</script>
---
+++ index.js
@@ -0,0 +1,8 @@
+import './style.css'
+import moment from 'vue-moment'
+
+export default {
+  install (Vue) {
+    Vue.use(moment)
+  }
+}
